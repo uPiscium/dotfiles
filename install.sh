e58efccb6b2116c10961bf018313f10f898e2d1d
uPiscium <upiscium@gmail.com.>
2024-07-12 20:13:39 +0900

    update

--- install.sh
+++ install.sh
@@ -12,12 +12,6 @@ ln -sf ~/dotfiles/.zshrc.local ~/.zshrc.local
 
 sudo cp ~/dotfiles/pacman.conf /etc/pacman.conf
 
-sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-sudo pacman -Syu
-
 sudo pacman -S --noconfirm git go
 
 cd ~/
@@ -38,7 +32,6 @@ cat ./packages.list | yay -S --noconfirm -
 sudo systemctl enable lightdm
 sudo systemctl enable acpid
 sudo systemctl enable bluetooth
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 git config --global init.defaultBranch main
 git config --global credential.helper store
--- rog.sh
+++ rog.sh
@@ -1,3 +1,11 @@
+sudo cp ~/dotfiles/rogPacman.conf /etc/pacman.conf
+
+sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+sudo pacman -Syu
+
 sudo systemctl enable --now power-profiles-daemon.service
 sudo systemctl enable --now supergfxd
 sudo systemctl enable --now switcheroo-control
