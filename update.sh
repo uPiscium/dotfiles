580a49549c0f50032b2700181eff7381c0e2ca22
uPiscium <upiscium@gmail.com>
2024-08-08 12:16:41 +0900

    update

--- update.sh
+++ update.sh
@@ -5,9 +5,8 @@ ROG_LIST=~/dotfiles/rog.list
 
 echo -e "\033[1;32mUpdating 'packages.list'...\033[0m"
 yay -Qeq > ${CURRENT_LIST}
-diff --old-line-format='' --unchanged-line-format='' --new-line-format='%L' ${PACKAGES_LIST} ${CURRENT_LIST} > ${TEMP_LIST}
-diff --old-line-format='' --unchanged-line-format='' --new-line-format='%L' ${ROG_LIST} ${TEMP_LIST} > ${PACKAGES_LIST}
-sort ${PACKAGES_LIST} > ${TEMP_LIST}
+diff --old-line-format='' --unchanged-line-format='' --new-line-format='%L' ${ROG_LIST} ${CURRENT_LIST} > ${TEMP_LIST}
+sort ${TEMP_LIST} > ${PACKAGES_LIST}
 
 rm ${CURRENT_LIST}
 rm ${TEMP_LIST}
