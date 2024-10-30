e944c578b006589983b68cd14e6e7ff963edc12e
uPiscium <upiscium@gmail.com>
2024-10-30 14:18:56 +0900

    update

--- NVIDIA.sh
+++ NVIDIA.sh
@@ -2,3 +2,11 @@ sudo mv /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf.bak /etc/X11/xorg.co
 sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.tmp
 sudo mv /etc/lightdm/lightdm.conf.bak /etc/lightdm/lightdm.conf
 sudo mv /etc/lightdm/lightdm.conf.tmp /etc/lightdm/lightdm.conf.bak
+
+echo -ne "\033[1;32mRestart for applying? (Y/n)\033[0m";
+read answer;
+if [ "$answer" = "n" ]; then
+  echo "Restart your system to apply changes.";
+else
+  reboot
+fi
